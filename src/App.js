481b5f041727ec18921653d25983a16bc2eba5a1
rsua <foxboxr@gmail.com>
2020-12-17 16:48:43 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import CounterContainer from "./containers/CounterContainer"
+
+import {Route,Link} from "react-router-dom";
+import Home from "./components/Home"
+import About from "./components/About"
+import Profiles from "./components/Profiles"
+import History from "./components/History"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <ul>
+          <li><Link to="/">home</Link></li>
+          <li><Link to="/about">about</Link></li>
+          <li><Link to="/profiles">profiles</Link></li>
+          <li><Link to="/history">history</Link></li>
+        </ul>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/profiles" component={Profiles}  />
+        <Route path="/history" component={History}  />
+      </div>
   );
 }
 
